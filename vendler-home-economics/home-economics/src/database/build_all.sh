8ca5874fc587f5576def159a950194a088540bb7
Mattias Vendler <mattias.vendler@gmail.com>
2012-03-28 21:46:39 +0200

    Fixed stuff with bankimport

--- vendler-home-economics/home-economics/src/database/build_all.sh
+++ vendler-home-economics/home-economics/src/database/build_all.sh
@@ -2,6 +2,8 @@
 cd tables/
 ./create_tables.sh
 cd ../data
-#time mysql -h127.0.0.1 -P5521 -umsandbox -pmsandbox < insert.sql
+echo "Inserting data"
+time mysql -h127.0.0.1 -P5521 -umsandbox -pmsandbox < insert.sql
+echo "Inserting data done"
 cd ../procedures
 ./compile.sh
--- vendler-home-economics/web-client/src/main/java/se/vendler/webclient/client/panels/BankImportEntriesPanel.java
+++ vendler-home-economics/web-client/src/main/java/se/vendler/webclient/client/panels/BankImportEntriesPanel.java
@@ -75,24 +75,27 @@ public class BankImportEntriesPanel extends Composite {
         Label text = new Label(entries.getText());
         Label date = new Label(df.format(entries.getDate()));
         ListBox accountGroups = getAccountGroups(entries.getAccountGroupId());
-//        int accountGroupId = accountGroups.getSelectedIndex();
-//        accountGroups.setSelectedIndex(accountGroupId);
-        ListBox accounts = getAccounts(1);
+        int accountGroupId = entries.getAccountGroupId()!=null && !entries.getAccountGroupId().equals(0)?entries.getAccountGroupId():1;
+        ListBox accounts = getAccounts(accountGroupId,entries.getAccount());
         Label amount = new Label(entries.getAmount());
         ListBox addRemoveListBox = new ListBox();
         addRemoveListBox.addItem("Lägg till", "add");
         addRemoveListBox.addItem("Ta bort", "remove");
+        buildRow(rowNumber, text, date, accountGroups, accounts, amount, addRemoveListBox);
+        accountGroups.addChangeHandler(new AccountGroupSelectedListener(accountGroups, accounts, rowNumber));
+    }
+
+    private void buildRow(int rowNumber, Label text, Label date, ListBox accountGroups, ListBox accounts, Label amount, ListBox addRemoveListBox) {
         flexTable.setWidget(rowNumber, 1, text);
         flexTable.setWidget(rowNumber, 2, date);
         flexTable.setWidget(rowNumber, 3, accountGroups);
         flexTable.setWidget(rowNumber, 4, accounts);
         flexTable.setWidget(rowNumber, 5, amount);
         flexTable.setWidget(rowNumber, 6, addRemoveListBox);
-        accountGroups.addChangeHandler(new AccountGroupSelectedListener(accountGroups, accounts, rowNumber));
     }
 
 
-    private ListBox getAccounts(int accountGroupId) {
+    private ListBox getAccounts(int accountGroupId, final String selectedAccount) {
         final ListBox listBox = new ListBox();
         accountControllerAsync.getAccounts(accountGroupId, new AsyncCallback<List<Account>>() {
             @Override
@@ -102,9 +105,17 @@ public class BankImportEntriesPanel extends Composite {
 
             @Override
             public void onSuccess(List<Account> result) {
+                int i =1;
+                int selectedIndex = i;
                 for (Account account : result) {
+                    if (selectedAccount != null && selectedAccount.equals(account.getAccountName())) {
+                      selectedIndex=i;
+                    } else {
+                        i++;
+                    }
                     listBox.addItem(account.getAccountName(), account.getAccountNumber());
                 }
+                listBox.setSelectedIndex(selectedIndex);
             }
         });
         return listBox;
@@ -125,7 +136,7 @@ public class BankImportEntriesPanel extends Composite {
         public void onChange(ChangeEvent event) {
             int selectedIndex = accountGroups.getSelectedIndex();
             String value = accountGroups.getValue(selectedIndex);
-            accounts = getAccounts(Integer.valueOf(value));
+            accounts = getAccounts(Integer.valueOf(value),null);
             flexTable.setWidget(rowNumber, 4, accounts);
         }
     }
@@ -144,8 +155,8 @@ public class BankImportEntriesPanel extends Composite {
                 for (int i = 0; i < result.size(); i++) {
                     AccountGroup accountGroup = result.get(i);
                     listBox.addItem(accountGroup.getName(), accountGroup.getId() + "");
-                    if (accountGroupId != null && accountGroupId.intValue() == accountGroup.getId()) {
-                        selectedIndex=i+1;
+                    if ((accountGroupId != null && !accountGroupId.equals(0)) && accountGroupId.intValue() == accountGroup.getId()) {
+                        selectedIndex=i;
                     }
 
                 }
