072196f0e6f8d35fd96b91a9a67c8dbd87be81d5
Mattias Vendler <mattias.vendler@gmail.com>
2012-07-26 13:50:02 +0200

    snepp

--- vendler-home-economics/home-economics/src/database/procedures/compile.sh
+++ vendler-home-economics/home-economics/src/database/procedures/compile.sh
@@ -3,7 +3,7 @@ FILES=*.sql
 for f in *.sql
 do
     echo $f
-    time mysql -h127.0.0.1 -P5520 -umsandbox -pmsandbox < $f
+    time mysql -hlocalhost -P5520 -umsandbox -pmsandbox < $f
 done
 
 #mysql -h127.0.0.1 -P5520 -umsandbox -pmsandbox < *.sql
